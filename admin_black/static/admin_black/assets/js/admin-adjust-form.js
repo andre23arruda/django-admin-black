34c267ba89358436abea1f806643b8ac05a5124e
André Arruda <33252657+andre23arruda@users.noreply.github.com>
2021-04-04 15:55:17 -0300

    Update admin-adjust-form.js
    Adjust input with

--- admin_black/static/admin_black/assets/js/admin-adjust-form.js
+++ admin_black/static/admin_black/assets/js/admin-adjust-form.js
@@ -1,6 +1,11 @@
 var form_inputs = document.querySelectorAll('form .col-md-7')
 form_inputs.forEach(input => {
     input.classList.remove('col-md-7')
-    input.classList.add('col-12')
+    input.classList.add('col-11')
+})
 
-});
+var dynamic_arrays = document.querySelectorAll('.dynamic-array-widget')
+dynamic_arrays.forEach(dynamic_array => {
+    dynamic_array.parentElement.classList.add('width-100')
+    dynamic_array.firstChild.classList.add('width-100')
+})
